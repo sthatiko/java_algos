c29a471150df4dc4b81d6707f17f1d8071d83edf
shravan_thatikonda <shravan.thatikonda@oracle.com>
2019-08-13 16:08:44 +0530

    reverse a singly linked list - v2

--- src/com/sthatiko/adhoc/SinglyLinkedListMain.java
+++ src/com/sthatiko/adhoc/SinglyLinkedListMain.java
@@ -15,15 +15,17 @@ public class SinglyLinkedListMain {
 }
 
     public static <T> void reverse(SinglyLinkedList<T> list){
-        if(list.isEmpty() || list.size == 1) return;
-        SinglyLinkedList.Node left = list.headNode, middle = left.nextNode, right = middle.nextNode;
-        left.nextNode = null;
-        while(middle != null){
-            middle.nextNode = left;
-            left = middle;
-            middle = right;
-            if(right != null) right=right.nextNode;
+        SinglyLinkedList<T>.Node prev = null; //To keep track of the previous element, will be used in swapping links
+        SinglyLinkedList<T>.Node current = list.headNode; //firstElement
+        SinglyLinkedList<T>.Node next = null;
+        //While Traversing the list, swap links
+        while (current != null) {
+            next = current.nextNode;
+            current.nextNode = prev;
+            prev = current;
+            current = next;
         }
-        list.headNode = left;
+        //Linking Head Node with the new First Element
+        list.headNode = prev;
     }
 }
